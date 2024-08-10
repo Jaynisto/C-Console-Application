ab8952ea0379be8f93db437e69cc5ab0da615e5d
fanie <faniejohnsons98@gmail.com>
2024-08-10 13:28:54 +0200

    Latest Commits

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+string name = "Fanie";
+int age = 26;
+string profession = "Software Developer";
+string skills = "C#, JavaScript, Linux, Agile.";
+string location = "Johannesburg, South Africa";
+string contact = "faniejohnsons98@gmail.com";
+
+Console.WriteLine("=======================================");
+Console.WriteLine("          Personal Information         ");
+Console.WriteLine("=======================================");
+Console.WriteLine();
+Console.WriteLine($"Name:          {name}");
+Console.WriteLine($"Age:           {age}");
+Console.WriteLine($"Profession:    {profession}");
+Console.WriteLine($"Skills:        {skills}");
+Console.WriteLine($"Location:      {location}");
+Console.WriteLine($"Contact:       {contact}");
+Console.WriteLine();
+Console.WriteLine("=======================================");
+Console.WriteLine("           Thank you for visiting!     ");
+Console.WriteLine("=======================================");
+    
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acf46e9e8ffa5e5d06d3e2f37a70ac802ae6bbe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
